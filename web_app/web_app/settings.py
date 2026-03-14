1f13bdbbf864ead3e4654b78ac4f50139596b9dc
Natasikk <natali_petrova_04@bk.ru>
2026-03-14 17:54:50 +0300

    Создание приложений

--- web_app/web_app/settings.py
+++ web_app/web_app/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.accounts.apps.AccountsConfig',
+    'apps.diary.apps.DiaryConfig',
+    'apps.habits.apps.HabitsConfig',
 ]
 
 MIDDLEWARE = [
