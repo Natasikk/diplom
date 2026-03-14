ea251e90e490ac215180a1bb5ade60c219d0a43a
Natasikk <natali_petrova_04@bk.ru>
2026-03-14 21:12:44 +0300

    Создание моделей для приложения Accounts

--- web_app/apps/accounts/models.py
+++ web_app/apps/accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import FileExtensionValidator
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    phone = models.CharField(max_length=20, verbose_name='Телефон')
+    birth_date = models.DateField(null=True, blank=True, verbose_name='Дата Рождения')
+    avatar = models.ImageField(verbose_name='Аватар', upload_to='images/avatars/',
+                               default='images/avatars/default.jpg',
+                               validators=[FileExtensionValidator(allowed_extensions=('png', 'jpg', 'jpeg'))])
+
+    class Meta:
+        verbose_name = "Профиль"
+        verbose_name_plural = "Профили"
+
+    def __str__(self):
+        return self.user.username
