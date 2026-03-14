22c7754571403c5cef5caa28af71e3bf656e047e
Natasikk <natali_petrova_04@bk.ru>
2026-03-14 23:40:26 +0300

    Создание моделей в приложении diary

--- web_app/apps/diary/admin.py
+++ web_app/apps/diary/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Emotion, Tag, DiaryEntry
 
-# Register your models here.
+@admin.register(Emotion)
+class EmotionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active')
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'created')
+
+@admin.register(DiaryEntry)
+class DiaryEntryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'emotion', 'title', 'created', 'updated')
+
--- web_app/apps/diary/models.py
+++ web_app/apps/diary/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Emotion(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Название эмоции')
+    emoji = models.CharField(max_length=10, verbose_name='Эмодзи')
+    is_active = models.BooleanField(default=True, verbose_name='Активна')
+
+    class Meta:
+        verbose_name='эмоция'
+        verbose_name_plural = 'Эмоции'
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название тега')
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, verbose_name='Пользователь')
+    is_system = models.BooleanField(default=False, verbose_name='Системный тег')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    class Meta:
+        verbose_name='тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
+
+class DiaryEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    date = models.DateField(verbose_name='Дата')
+    emotion = models.ForeignKey(Emotion, on_delete=models.CASCADE, verbose_name='Настроение дня')
+    tags = models.ManyToManyField(Tag, blank=True, verbose_name='Теги дня')
+    title = models.CharField(max_length=200, default='Без названия', verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Содержание записи')
+    achievements = models.TextField(blank=True, null=True, verbose_name='Достижения дня')
+    difficulties = models.TextField(blank=True, null=True, verbose_name='Трудности дня')
+    tomorrow_plans = models.TextField(blank=True, null=True, verbose_name='Планы на завтра')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата и время создания')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Дата и время обновления')
+
+    class Meta:
+        verbose_name = 'запись'
+        verbose_name_plural = 'Записи'
+        ordering = ['-date', '-created']
+
+    def __str__(self):
+        date_str = self.date.strftime("%d.%m.%Y")
+        title = self.title
+        return f"{date_str} - {title}"
